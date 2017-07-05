32cb5dd8e91fed869b18a974a7454ebf12ff71b1
子贤 <1148218318@qq.com>
2017-07-05 10:23:08 +0800

    端口号修改为10000 ==》 11187

--- config/index.js
+++ config/index.js
@@ -23,7 +23,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 10000,
+    port: 11187,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
